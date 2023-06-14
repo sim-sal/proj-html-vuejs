f11209547aac7a473e6da98b28d617e620748a35
sim-sal <simone.saladino93@gmail.com>
2023-06-14 12:05:14 +0200

    added div hover jumbotron

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -15,7 +15,7 @@ export default {
     <main>
         <!-- jumbotron -->
         <div class="jumbotron">
-            <div class="container">
+            <div class="container jumbo">
                 <div class="text_jumbo">
 
                     <!-- titolo -->
@@ -33,6 +33,42 @@ export default {
                     </div>
 
                 </div>
+
+            </div>
+
+            <div class="input_counter">
+                <div class="row align-items-center row_input_counter">
+                    <div class="col-6 text-center">
+                        <h3 class="discount"><span class="limited_sale">Limited Sale:</span> All courses with 55% off</h3>
+                        <div class="input-group mb-1">
+                            <input type="text" class="form-control" placeholder="Enter your E-mail"
+                                aria-label="Recipient's username" aria-describedby="button-addon2">
+                            <button class="btn btn-outline-secondary" type="button" id="button-addon2">SUBSCRIBE</button>
+                        </div>
+                    </div>
+
+                    <div class="col-6">
+                        <div class="container_counter text-center">
+                            <div>
+                                <h1 class="numbers">26</h1>
+                                <span><strong>Days</strong></span>
+                            </div>
+                            <div>
+                                <h1 class="numbers">01</h1>
+                                <span><strong>Hours</strong></span>
+                            </div>
+                            <div>
+                                <h1 class="numbers">47</h1>
+                                <span><strong>Minutes</strong></span>
+                            </div>
+                            <div>
+                                <h1 class="numbers">34</h1>
+                                <span><strong>Seconds</strong></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
         </div>
@@ -55,8 +91,9 @@ main {
         height: 650px;
         background-image: url('/jumbotron.jpg');
         background-size: cover;
+        position: relative;
 
-        .container {
+        .jumbo {
             height: 650px;
             display: flex;
             align-items: center;
@@ -78,6 +115,51 @@ main {
 
                 #revolution {
                     color: $footer-color;
+                }
+            }
+
+        }
+
+        .input_counter {
+            width: 65%;
+            height: 180px;
+            border-radius: 10px;
+            position: absolute;
+            background-color: $footer-color;
+            top: 520px;
+            left: 260px;
+
+            .row_input_counter {
+                height: 180px;
+
+                .discount {
+                    font-weight: bold;
+                }
+
+                .limited_sale {
+                    color: white;
+                }
+
+                .input-group {
+
+                    width: 90%;
+                    margin: 20px auto;
+
+                    button {
+                        color: white;
+                        background-color: rgb(45, 70, 73);
+                        border-radius: 20px;
+                    }
+                }
+            }
+
+            .container_counter {
+                @include flex-around;
+
+                .numbers {
+                    color: white;
+                    font-weight: bold;
+                    font-size: 60px;
                 }
             }
         }
